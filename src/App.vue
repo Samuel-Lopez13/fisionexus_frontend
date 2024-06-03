712f28816831b6e803895c082010aabfd6758fd0
Samuel-Lopez13 <lopez180219@gmail.com>
2024-06-03 11:58:02 -0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+</script>
+
+<template>
+    <div class="flex">
+        <h3 class="w-1/12 bg-blue-200">Hola</h3>
+        <h3 class="w-2/12 bg-blue-200">Hola</h3>
+        <h3 class="w-2/12 bg-blue-200">Hola</h3>
+        <h3 class="w-2/12 bg-blue-200">Hola</h3>
+        <h3 class="w-2/12 bg-blue-200">Hola</h3>
+        <h3 class="w-2/12 bg-blue-200">Hola</h3>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
