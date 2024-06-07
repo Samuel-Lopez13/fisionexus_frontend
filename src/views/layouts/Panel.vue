466afd70245b47f8d7421685333879930737e354
Edson <craes20@hotmail.com>
2024-06-07 14:25:38 -0600

    Se completo el layout panel

---
+++ src/views/layouts/Panel.vue
@@ -0,0 +1,20 @@
+<script setup>
+import Sidebar from '@/views/layouts/Sidebar.vue'
+import Header from '@/views/layouts/Header.vue'
+</script>
+
+<template>
+  <div class="flex overflow-hidden">
+    <Sidebar/>
+    <div class="flex flex-col w-full">
+      <Header/>
+      <div>
+        <router-view></router-view>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
