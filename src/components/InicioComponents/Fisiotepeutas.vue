3c4967018c407bead277940784d15a91e4a6be47
Edson <craes20@hotmail.com>
2024-06-10 10:39:49 -0600

    Tamano usuairo y notificaciones

---
+++ src/components/InicioComponents/Fisiotepeutas.vue
@@ -0,0 +1,42 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="w-full max-w-md p-4 overflow-x-auto bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+    <div class="flex items-center justify-between mb-3
+">
+      <h5 class="text-xl font-bold leading-none text-blue-600">Fisioterapeutas</h5>
+      <a href="#" class="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500">
+        Ver a todos
+      </a>
+    </div>
+    <div class="flow-root">
+      <ul role="list" class="divide-y divide-gray-200 dark:divide-gray-700">
+        <li class="py-3 sm:py-4">
+          <div class="flex items-center">
+            <div class="flex-shrink-0">
+              <img class="w-8 h-8 rounded-full object-cover" src="https://img.freepik.com/foto-gratis/retrato-hermoso-mujer-joven-posicion-pared-gris_231208-10760.jpg" alt="Neil image">
+            </div>
+            <div class="flex-1 min-w-0 ms-4 mr-3">
+              <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
+                Samuel Dzec Pat
+              </p>
+              <p class="text-sm text-gray-500 truncate dark:text-gray-400">
+                samuelSharp@gmail.com
+              </p>
+            </div>
+            <div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+              9812038412
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
