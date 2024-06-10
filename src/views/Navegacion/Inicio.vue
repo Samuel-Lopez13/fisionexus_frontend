7bd44a0abd82bfabc0987aca971361c2e01042c2
Edsonn7 <craes20@hotmail.com>
2024-06-10 14:41:51 -0600

    Tabla Ultimos usuarios

--- src/views/Navegacion/Inicio.vue
+++ src/views/Navegacion/Inicio.vue
@@ -38,7 +38,7 @@ const Saludar = () => {
         <CardCita />
       </div>
     </section>
-    <section class="flex telefono:flex-col py-2 overflow-x-auto telefono:gap-4 tablet:gap-4 laptop:gap-5">
+    <section class="flex telefono:flex-col py-2 style_scroll overflow-x-auto telefono:gap-4 tablet:gap-4 laptop:gap-5">
       <div class="telefono:flex telefono:justify-center">
         <NuevosUsuarios />
       </div>
