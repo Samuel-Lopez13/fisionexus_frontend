5dd08bb2f36c302b9decd0996211b946a170a859
Edson <craes20@hotmail.com>
2024-06-12 17:28:27 -0600

    Mejoras en agregar paciente

--- src/router/index.js
+++ src/router/index.js
@@ -12,7 +12,7 @@ const router = createRouter({
    history: createWebHistory(import.meta.env.BASE_URL),
    routes: [
       {
-         path: '/Login',
+         path: '/',
          name: 'login',
          component: Login,
          meta: { requiresAuth: false, verifyToken: false },
