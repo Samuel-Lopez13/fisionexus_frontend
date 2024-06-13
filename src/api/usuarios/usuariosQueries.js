1a6a323e5b82cbe4f0ee4a47db402a896358fbee
Edson <craes20@hotmail.com>
2024-06-13 02:21:46 -0600

    sda

--- src/api/usuarios/usuariosQueries.js
+++ src/api/usuarios/usuariosQueries.js
@@ -4,14 +4,13 @@ import { autorization } from '@/api/headers.js'
 const apiUrl = import.meta.env.VITE_API_URL;
 
 export const usuariosQueries = {
-
    verifyUser: async (token) => {
-      try{
-         const response = await axios.get(apiUrl + "/User/verifyUser/" + token, autorization())
-
-         return response.data
-      } catch (error){
+      try {
+         const response = await axios.get(apiUrl + "/User/verifyUser/" + token, autorization());
+         return response.data;
+      } catch (error) {
+         console.error('Verification failed:', error);
+         return null;  // Retorna null o un objeto con verify: false en caso de error
       }
    }
-
-}
+};
--- src/router/index.js
+++ src/router/index.js
@@ -6,103 +6,118 @@ import LayoutInicio from '@/views/Navegacion/Inicio/LayoutInicio.vue'
 import LayoutPacientes from '@/views/Navegacion/Pacientes/LayoutPacientes.vue'
 import LayoutMetricas from '@/views/Navegacion/Metricas/LayoutMetricas.vue'
 import AgregarPaciente from '@/views/Navegacion/Pacientes/AgregarPaciente.vue'
-import { verify } from '@/services/verifyToken.js'
 import LayoutAjustes from '@/views/Navegacion/Ajustes/LayoutAjustes.vue'
+import { usuariosQueries } from '@/api/usuarios/usuariosQueries.js'
 
+const routes = [
+   {
+      path: '/',
+      name: 'login',
+      component: Login,
+      meta: { verifyToken: false }
+   },
+   {
+      path: '/',
+      name: 'Panel',
+      component: Panel,
+      meta: { verifyToken: true},
+      children: [
+         {
+            path: 'Inicio',
+            name: 'Inicio',
+            component: Navegacion,
+            meta: {title: 'Dashboard - Fisiolabs'},
+            redirect: { name: 'Dashboard' },
+            children: [
+               {
+                  path: '',
+                  name: 'Dashboard',
+                  component: LayoutInicio
+               }
+            ]
+         },
+         {
+            path: 'Pacientes',
+            name: 'Pacientes',
+            component: Navegacion,
+            redirect: { name: 'ListaPacientes' },
+            meta: {title: 'Pacientes - Fisiolabs'},
+            children: [
+               {
+                  path: '',
+                  name: 'ListaPacientes',
+                  component: LayoutPacientes
+               },
+               {
+                  path: 'AgregarPaciente',
+                  name: 'AgregarPaciente',
+                  component: AgregarPaciente,
+               }
+            ]
+         },
+         {
+            path: 'Metricas',
+            name: 'Metricas',
+            component: Navegacion,
+            meta: {title: 'Metricas - Fisiolabs'},
+            redirect: { name: 'Estadisticas' },
+            children: [
+               {
+                  path: '',
+                  name: 'Estadisticas',
+                  component: LayoutMetricas
+               }
+            ]
+         },
+         {
+            path: 'Ajustes',
+            name: 'Ajustes',
+            component: Navegacion,
+            meta: {title: 'Ajustes - Fisiolabs'},
+            redirect: { name: 'Configuracion' },
+            children: [
+               {
+                  path: '',
+                  name: 'Configuracion',
+                  component: LayoutAjustes
+               }
+            ]
+         }
+      ]
+   }
+];
 const router = createRouter({
    history: createWebHistory(import.meta.env.BASE_URL),
-   routes: [
-      {
-         path: '/',
-         name: 'login',
-         component: Login,
-         meta: { verifyToken: false }
-      },
-      {
-         path: '/',
-         name: 'Panel',
-         component: Panel,
-         meta: { verifyToken: true},
-         children: [
-            {
-               path: 'Inicio',
-               name: 'Inicio',
-               component: Navegacion,
-               meta: {title: 'Dashboard - Fisiolabs'},
-               redirect: { name: 'Dashboard' },
-               children: [
-                  {
-                     path: '',
-                     name: 'Dashboard',
-                     component: LayoutInicio
-                  }
-               ]
-            },
-            {
-               path: 'Pacientes',
-               name: 'Pacientes',
-               component: Navegacion,
-               redirect: { name: 'ListaPacientes' },
-               meta: {title: 'Pacientes - Fisiolabs'},
-               children: [
-                  {
-                     path: '',
-                     name: 'ListaPacientes',
-                     component: LayoutPacientes
-                  },
-                  {
-                     path: 'AgregarPaciente',
-                     name: 'AgregarPaciente',
-                     component: AgregarPaciente,
-                  }
-               ]
-            },
-            {
-               path: 'Metricas',
-               name: 'Metricas',
-               component: Navegacion,
-               meta: {title: 'Metricas - Fisiolabs'},
-               redirect: { name: 'Estadisticas' },
-               children: [
-                  {
-                     path: '',
-                     name: 'Estadisticas',
-                     component: LayoutMetricas
-                  }
-               ]
-            },
-            {
-               path: 'Ajustes',
-               name: 'Ajustes',
-               component: Navegacion,
-               meta: {title: 'Ajustes - Fisiolabs'},
-               redirect: { name: 'Configuracion' },
-               children: [
-                  {
-                     path: '',
-                     name: 'Configuracion',
-                     component: LayoutAjustes
-                  }
-               ]
-            }
-         ]
-      }
-   ]
+   routes
 })
 
-router.beforeEach( (to, from, next) => {
-
-   /*Verifica que el token sea autentico*/
-   if (to.meta.verifyToken) {
-      const response = verify()
-
-      if (response.verify === false) {
-         localStorage.removeItem(import.meta.env.VITE_CREDENCIALES)
-         next('/')
-      } else{
-         next()
+router.beforeEach( async (to, from, next) => {
+      if (to.meta.verifyToken) {
+         const token = localStorage.getItem(import.meta.env.VITE_CREDENCIALES);
+         if (!token) {
+            next('/');
+         } else {
+            usuariosQueries.verifyUser(token)
+              .then(response => {
+                 console.log(response + 'then')
+                 if (!response || response.verify === false) {
+                    console.log(response + 'en if')
+                    localStorage.removeItem(import.meta.env.VITE_CREDENCIALES);
+                    next('/');
+                 } else {
+                    console.log(response + 'En else')
+                    next();
+                 }
+              })
+              .catch(error => {
+                 console.error('Error during token verification:', error);
+                 localStorage.removeItem(import.meta.env.VITE_CREDENCIALES);
+                 next('/');
+              });
+         }
+      } else {
+         next();
       }
-   }
 
    /*Cambia el titulo dependiendo de la pagina*/
    if(to.meta.title){
