423bcd53539d8d7f95ae0b6837eea9fd7099bf41
Samuel-Lopez13 <lopez180219@gmail.com>
2024-06-18 22:48:27 -0600

    instalar flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
+import flowbitePlugin from 'flowbite/plugin';
 export default {
     content: [
         "./index.html",
         "./src/**/*.{vue,js,ts,jsx,tsx}",
+        "./node_modules/flowbite/**/*.js"
     ],
     theme: {
         extend: {
@@ -45,6 +47,8 @@ export default {
             }
         },
     },
-    plugins: [],
+    plugins: [
+        flowbitePlugin
+    ],
 }
 
