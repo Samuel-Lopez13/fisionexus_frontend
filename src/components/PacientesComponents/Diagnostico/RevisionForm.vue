9c5aa50298fada5888462c5ba2e869d2a9c917cc
Edson <craes20@hotmail.com>
2024-08-08 18:26:57 -0600

    Nuevas modificaciones

--- src/components/PacientesComponents/Diagnostico/RevisionForm.vue
+++ src/components/PacientesComponents/Diagnostico/RevisionForm.vue
@@ -18,7 +18,13 @@ import SignosPost from '@/components/PacientesComponents/SignosVitales/SignosPos
                 <p>Comprobante de pago <span class="text-blue-600">*</span></p>
                 <input type="text" class="input-primary" placeholder="Escribe aqui el folio de pago">
                 <p>Observaciones <span class="text-blue-600">*</span></p>
-                <textarea name="" id="revision" cols="30" placeholder="Escribe aqui tus observaciones de la revision" rows="13" class="input-primary resize-none"></textarea>
+                <textarea name="" id="revision" cols="30" placeholder="Escribe aqui tus observaciones de la revision" rows="9" class="input-primary resize-none"></textarea>
+                <p>Servicios <span class="text-blue-600">*</span></p>
+                <select name="" id="" class="input-primary">
+                    <option value="">Ninguno</option>
+                    <option value="">Hidrobano</option>
+                    <option value="">Masaje</option>
+                </select>
                 <button id="agendar-btn" class="rounded-[3px] p-2 bg-blue-600 text-white w-full hover:bg-pHover">Subir revisión</button>
             </section>
         </div>
